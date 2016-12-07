655d6873aef36afb1f9aa873230a8a48d22de0ae
AKabhishek <abhishekkaushik.star@gmail.com>
2016-12-07 06:38:03 +0000

    Disable Doze

--- msm8956.mk
+++ msm8956.mk
@@ -124,8 +124,8 @@ PRODUCT_PACKAGES += \
     liboverlay
 
 # Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
+#PRODUCT_PACKAGES += \
+#    XiaomiDoze
 
 # Face detection extension
 PRODUCT_PACKAGES += \

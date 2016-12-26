d5d35612746cd0b2b55885c0ad631fccbdb3432e
xyyx <xyyx@mail.ru>
2016-12-26 19:01:01 +0800

    Prepared for Nitrogen

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -177,7 +177,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # RIL
 PROTOBUF_SUPPORTED := true
-TARGET_RIL_VARIANT := caf
+#TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk

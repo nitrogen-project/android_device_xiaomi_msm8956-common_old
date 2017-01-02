599a0d1b5a29b601ac7b20484d046934bd2aa25c
xyyx <xyyx@mail.ru>
2017-01-02 18:43:07 +0800

    Use Impulse governor
    Change-Id: I01ab0cd86bab45c6d7b9b558df35b42cbeeb8bce

--- configs/init.qcom.post_boot.sh
+++ configs/init.qcom.post_boot.sh
@@ -6,8 +6,8 @@
 # Governors
 # Enable governor for power cluster
  echo 1 > /sys/devices/system/cpu/cpu0/online
- echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+ echo "impulse" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
 # Enable governor for perf cluster
  echo 1 > /sys/devices/system/cpu/cpu4/online
- echo "interactive" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+ echo "impulse" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
